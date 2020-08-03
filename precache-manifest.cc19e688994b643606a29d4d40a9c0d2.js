2b1f46d2c3c7e62fe8e4196bbe2777b2cddd11e9
Maximuss Hùng <vidostartup@gmail.com>
2020-08-03 21:46:18 +0700

    Updates

---
+++ precache-manifest.cc19e688994b643606a29d4d40a9c0d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cf9bc46584b5062274574ebb92edf3b",
+    "url": "/newenglish/index.html"
+  },
+  {
+    "revision": "78f8bf534ba13ceaba0f",
+    "url": "/newenglish/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6d3f557804079aca58d9",
+    "url": "/newenglish/static/js/2.0e7246ce.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/newenglish/static/js/2.0e7246ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78f8bf534ba13ceaba0f",
+    "url": "/newenglish/static/js/main.fdea56e3.chunk.js"
+  },
+  {
+    "revision": "235906316c4b7bca3061",
+    "url": "/newenglish/static/js/runtime-main.2efb43b5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/newenglish/static/media/logo.5d5d9eef.svg"
+  }
+]);
