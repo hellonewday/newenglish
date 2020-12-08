a51e991f356260737fc2b37dba490c8e534019a2
Maximuss Hùng <vidostartup@gmail.com>
2020-12-08 10:09:05 +0700

    Updates

---
+++ precache-manifest.6376a61a4ba56a1ab0cc052eef66d7c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4df1465309d428b22035f83c7f122648",
+    "url": "/newenglish/index.html"
+  },
+  {
+    "revision": "4a1819f8de0d269ca38a",
+    "url": "/newenglish/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d74e84c819c4d13590e2",
+    "url": "/newenglish/static/js/2.e5c67153.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/newenglish/static/js/2.e5c67153.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a1819f8de0d269ca38a",
+    "url": "/newenglish/static/js/main.679a3cf8.chunk.js"
+  },
+  {
+    "revision": "235906316c4b7bca3061",
+    "url": "/newenglish/static/js/runtime-main.2efb43b5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.b35ccbf903ecf41bcac261b003f765df.js"
+  "/newenglish/precache-manifest.6376a61a4ba56a1ab0cc052eef66d7c2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/newenglish/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
